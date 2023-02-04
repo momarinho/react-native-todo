740d70d75d349e8be11a7bba85cdf9e8a129ce41
momarinho <117489486+momarinho@users.noreply.github.com>
2023-02-03 21:09:47 -0300

    Update
    Still need to fix somethings about the visual part

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import {
   View,
   Text,
   StyleSheet,
+  ScrollView,
   TouchableOpacity,
   TextInput,
 } from 'react-native';
@@ -48,50 +49,54 @@ const TodoApp = () => {
   return (
     <>
       <StatusBar style="light" />
-      <View style={styles.container}>
-        {editMode ? (
-          <TextInput
-            style={styles.input}
-            value={editedTodo}
-            onChangeText={(text) => setEditedTodo(text)}
-            placeholder="Edit the todo"
-          />
-        ) : (
-          <TextInput
-            style={styles.input}
-            value={newTodo}
-            onChangeText={(text) => setNewTodo(text)}
-            placeholder="Add a new todo"
-          />
-        )}
-        <TouchableOpacity style={styles.button} onPress={addTodo}>
-          <Text style={styles.buttonText}>
-            {editMode ? 'Save Changes' : 'Add Todo'}
-          </Text>
-        </TouchableOpacity>
-        {todos.map((todo, index) => (
-          <View key={index} style={styles.todoContainer}>
-            <TouchableOpacity onPress={() => markTodo(index)}>
-              <Text
-                style={{
-                  ...styles.todoText,
-                  textDecorationLine: todo.completed ? 'line-through' : 'none',
-                }}
-              >
-                {todo.text}
-              </Text>
-            </TouchableOpacity>
-            <View style={styles.actions}>
-              <TouchableOpacity onPress={() => editTodo(index)}>
-                <Text style={styles.actionText}>Edit</Text>
+      <ScrollView>
+        <View style={styles.container}>
+          {editMode ? (
+            <TextInput
+              style={styles.input}
+              value={editedTodo}
+              onChangeText={(text) => setEditedTodo(text)}
+              placeholder="Edit the todo"
+            />
+          ) : (
+            <TextInput
+              style={styles.input}
+              value={newTodo}
+              onChangeText={(text) => setNewTodo(text)}
+              placeholder="Add a new todo here..."
+            />
+          )}
+          <TouchableOpacity style={styles.button} onPress={addTodo}>
+            <Text style={styles.buttonText}>
+              {editMode ? 'Save Changes' : 'Add Todo'}
+            </Text>
+          </TouchableOpacity>
+          {todos.map((todo, index) => (
+            <View key={index} style={styles.todoContainer}>
+              <TouchableOpacity onPress={() => markTodo(index)}>
+                <Text
+                  style={{
+                    ...styles.todoText,
+                    textDecorationLine: todo.completed
+                      ? 'line-through'
+                      : 'none',
+                  }}
+                >
+                  {todo.text}
+                </Text>
               </TouchableOpacity>
-              <TouchableOpacity onPress={() => deleteTodo(index)}>
-                <Text style={styles.actionText}>Delete</Text>
-              </TouchableOpacity>
+              <View style={styles.actions}>
+                <TouchableOpacity onPress={() => editTodo(index)}>
+                  <Text style={styles.editText}>Edit</Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => deleteTodo(index)}>
+                  <Text style={styles.deleteText}>Delete</Text>
+                </TouchableOpacity>
+              </View>
             </View>
-          </View>
-        ))}
-      </View>
+          ))}
+        </View>
+      </ScrollView>
     </>
   );
 };
@@ -101,39 +106,64 @@ export default TodoApp;
 const styles = StyleSheet.create({
   container: {
     padding: 40,
+    width: '100%',
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#18180E',
   },
   input: {
-    height: 40,
-    borderColor: 'gray',
+    height: 50,
+    backgroundColor: '#fff',
+    borderColor: '#ccc',
     borderWidth: 1,
     marginBottom: 20,
     padding: 10,
+    width: '100%',
+    borderRadius: 8,
   },
   button: {
-    backgroundColor: 'blue',
-    padding: 10,
-    borderRadius: 5,
+    backgroundColor: '#007aff',
+    padding: 12,
+    borderRadius: 8,
+    width: '100%',
+    alignSelf: 'flex-end',
   },
   buttonText: {
-    color: 'white',
+    color: '#fff',
     fontWeight: 'bold',
+    textTransform: 'uppercase',
+    fontSize: 16,
+    textAlign: 'center',
   },
   todoContainer: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     marginTop: 20,
     padding: 10,
-    borderColor: 'gray',
+    borderColor: '#ccc',
     borderWidth: 1,
+    width: '100%',
+    borderRadius: 8,
+    alignItems: 'center',
+    backgroundColor: '#CEBCB8',
   },
   todoText: {
     fontSize: 18,
+    color: '#333',
   },
   actions: {
     flexDirection: 'row',
   },
-  actionText: {
+  editText: {
     marginLeft: 10,
-    color: 'blue',
+    color: '#007aff',
+    fontWeight: 'bold',
+    color: '#7B89AE',
+  },
+  deleteText: {
+    marginLeft: 10,
+    color: '#007aff',
+    fontWeight: 'bold',
+    color: '#b9080e',
   },
 });
